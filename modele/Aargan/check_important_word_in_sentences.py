3afef1dde60c60851eb329646834bdf4c655aac2
AarganC <aargan.cointepas.pro@gmail.com>
2020-01-17 02:16:49 +0100

    Ajout du fichier files/top_word.txt et modele/Aargan/check_important_word_in_sentences.py est fonctionnel

--- modele/Aargan/check_important_word_in_sentences.py
+++ modele/Aargan/check_important_word_in_sentences.py
@@ -21,37 +21,49 @@ for file in files:
         corpus.append(tmp)
     f.close()
 
-sentences = []
+final_sentences = []
 for sentence in corpus:
-    sentences.append(nltk.sent_tokenize(sentence))
+    sentences = nltk.sent_tokenize(sentence)
 
-print(sentences[:1])
+    print(sentences[:5])
 
-clean_sentences = []
-clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Zàéêèç]", " ") # ("/^[a-zàâçéèêëîïôûùüÿñæœ .-]*$/i" , " ")
-clean_sentences = [print(sentence) for sentence in clean_sentences]
+    clean_sentences = []
+    clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Zàéêèç]", " ") # ("/^[a-zàâçéèêëîïôûùüÿñæœ .-]*$/i" , " ")
+    clean_sentences = [sentence.lower() for sentence in clean_sentences]
 
-print(clean_sentences[:1])
 
-stop_words = stopwords.words('french')
-def remove_stopwords(sen):
-    sen_new = " ".join([i for i in sen if i not in stop_words])
-    return sen_new
 
-clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
+    stop_words_fr = stopwords.words('french')
+    stop_words_en = stopwords.words('english')
+    def remove_stopwords_fr(sen):
+        sen_new = " ".join([i for i in sen if i not in stop_words_fr])
+        return sen_new
 
-print(2)
+    def remove_stopwords_en(sen):
+        sen_new = " ".join([i for i in sen if i not in stop_words_en])
+        return sen_new
+
+    clean_sentences = [remove_stopwords_fr(r.split()) for r in clean_sentences]
+    clean_sentences = [remove_stopwords_en(r.split()) for r in clean_sentences]
+    final_sentences.append(clean_sentences)
+
+
+print(str(2) + str(type(final_sentences)))
 
 wordfreq = {}
-for sentence in clean_sentences:
-    tokens = nltk.word_tokenize(sentence)
-    for token in tokens:
-        if token not in wordfreq.keys():
-            wordfreq[token] = 1
-        else:
-            wordfreq[token] += 1
+for sentences in final_sentences:
+    for sentence in sentences:
+        if detect(sentence) == "fr":
+            tokens = nltk.word_tokenize(sentence)
+            for token in tokens:
+                if token not in wordfreq.keys():
+                    wordfreq[token] = 1
+                else:
+                    wordfreq[token] += 1
 
 print(3)
 
 most_freq = heapq.nlargest(200, wordfreq, wordfreq.get)
-print(most_freq[:10])
+f = open("resultat/top_word.txt", "w")
+f.write(str(most_freq))
+f.close()
